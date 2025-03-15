315d651ee922a612089b07e325f90991efb5dffb
chrisk123999 <chrisk123999@gmail.com>
2025-03-15 01:39:23 -0700

    Sync

---
+++ scripts/module.js
@@ -0,0 +1,7 @@
+import * as macros from './macros.js';
+import * as legacyMacros from './legacyMacros.js';
+import {macroUtils} from './utils.js';
+Hooks.once('ready', () => {
+    //macroUtils.registerMacros(macros);
+    //macroUtils.registerMacros(legacyMacros);
+});
